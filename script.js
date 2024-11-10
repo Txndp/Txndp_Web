13fe0af4ead85ffdb9abf53754ba653412ab7026
Txndp <150055126+Txndp@users.noreply.github.com>
2024-11-11 01:55:20 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function openPopup() {
+    // Show the popup
+    document.getElementById("popup").style.display = "block";
+    // Show the background with blur
+    document.getElementById("background").style.display = "block";
+}
+
+function closePopup() {
+    // Hide the popup
+    document.getElementById("popup").style.display = "none";
+    // Hide the background
+    document.getElementById("background").style.display = "none";
+}
